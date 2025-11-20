49cf4041431ee605278e2bc4e14617f98b38c7c3
Sourashis Ghosh Roy <personal.sourashisghoshroy@gmail.com>
2025-11-20 15:15:10 +0530

    Update Tasks

--- firebase_config.py
+++ firebase_config.py
@@ -1,11 +1,11 @@
 import requests
 
 # Base Firebase project details
-API_KEY = "YOUR_FIREBASE_API_KEY"
+API_KEY = "AIzaSyB8MsAaONyO3MRyQWfw5Qque6iaVtK4fjg"
 
 # Firebase Auth REST endpoints
 FIREBASE_AUTH_SIGNUP = f"https://identitytoolkit.googleapis.com/v1/accounts:signUp?key={API_KEY}"
 FIREBASE_AUTH_LOGIN = f"https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key={API_KEY}"
 
 # Your Realtime Database URL
-DATABASE_URL = "https://YOUR_PROJECT_ID.firebaseio.com"
+DATABASE_URL = "https://fundstack-cli-default-rtdb.firebaseio.com/"
